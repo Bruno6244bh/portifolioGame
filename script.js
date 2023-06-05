008d76b99699fbec449a34fd27361932ea6e440b
Bruno6244bh <bruno6244.bh@gmail.com>
2023-06-05 09:24:03 -0300

    redo tilemap and character movement

--- script.js
+++ script.js
@@ -1,123 +1,65 @@
-let img = new Image(); // criação do objeto imagem no javascript
-img.src = './sources/character.png'; // definição do source da imagem
-img.onload = function() { // função que será executada assim que o objeto "img" for carregado
-    init();
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = 1280;
+canvas.height = 736;
+
+context.fillStyle = 'white';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = './sources/mainTileset.png';
+
+const playerImage = new Image();
+playerImage.src = './sources/runDown.png'
+
+image.onload = () => {
+context.drawImage(image, 0, 0);
+context.drawImage(
+playerImage, 
+0,
+0,
+playerImage.width / 3,
+playerImage.height,
+canvas.width / 2 - playerImage.width / 2, 
+canvas.height / 2 - playerImage.height / 2,
+playerImage.width / 3,
+playerImage.height,)
 }
 
-let canvas = document.querySelector('canvas'); // referencia ao objeto "canvas" no javascript
-let context = canvas.getContext('2d');
-const escale = 1; // constant to scale character's size
-const Width = 48; // character's width
-const Height = 63; // character's height
-const escaleWidth = escale * Width;
-const escaleHeight = escale * Height;
-let positionX = 0; //variable that define character's position on the X axis
-let positionY = 0; //variable that define character's position on the Y axis
-let speed = 1.5 //variable that defines character's speed
+window.addEventListener('keydown', (e) => {
+    console.log('keydown = ' + e.keyCode)
+    switch (e.keyCode) {
+        case 87:
+            console.log('up')
+        break;
 
-//variables above define character's current direction 
-let up = false;
-let down = false;
-let right = false;
-let left = false;
+        case 38:
+            console.log('up')
+        break;
 
-function drawFrames(posX, posY, canvasX, canvasY) {
-    // context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(img, posX*Width, posY*Height, Width, Height, canvasX, canvasY, escaleWidth, escaleHeight);
-}
+        case 65:
+            console.log('left')
+        break;
 
-const images = [1, 0, 1, 2]; // sprites's order in spritesheet to animation
-let indexImage = 2; // array position
-let countingFrames = 0; // scoring position in spritesheet
-let currentDirection = 0; // sprite direction
+        case 37:
+            console.log('left')
+        break;
 
-function step() {
+        case 83:
+            console.log('down')
+        break;
 
-    cancelAnimationFrame(idle)
+        case 40:
+            console.log('down')
+        break;
 
-    countingFrames++;
-    if(countingFrames < 13) {
-        window.requestAnimationFrame(step);
-        return;
+        case 68:
+            console.log('right')
+        break;
+
+        case 39:
+            console.log('right')
+        break;
     }
-    countingFrames = 0;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrames(images[indexImage], currentDirection, positionX, positionY);
-    indexImage++;
-    if(indexImage >= images.length) {
-        indexImage = 0;
-    }
-    window.requestAnimationFrame(step);
-}
-
-
-function idle() {
-
-    if(currentDirection === 0) {
-        indexImage = 2;
-    } else if ( currentDirection === 1) {
-        indexImage = 5
-    } else if (currentDirection === 2) {
-        indexImage = 8
-    } else if (currentDirection === 3) {
-        indexImage = 11
-    }
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawFrames(images[indexImage], currentDirection, positionX, positionY);
-}
-
-
-function init() {
-    window.requestAnimationFrame(idle);
-}
-
-window.addEventListener('keydown', (event) => {
-    if (event.keyCode === 37) {
-        left = true;
-        step()
-    } else if (event.keyCode === 39) {
-        right = true;
-        step()
-    } else if (event.keyCode === 38) {
-        up = true
-        step()
-    } else if (event.keyCode === 40) {
-        down = true;
-        step()
-    } 
-} )
-
-window.addEventListener('keyup', (event) => {
-    if (event.keyCode === 37) {
-        left = false;
-    } else if (event.keyCode === 39) {
-        right = false;
-    } else if (event.keyCode === 38) {
-        up = false
-    } else if (event.keyCode === 40) {
-        down = false;
-    }
-} )
-
-const game = () => {
-    if (up === true) {
-        positionY -= speed;
-        currentDirection = 3
-    }
-    if (down === true) {
-        positionY += speed;
-        currentDirection = 0
-    }
-    if (left === true) {
-        positionX -= speed;
-        currentDirection = 1
-    }
-    if (right === true) {
-        positionX += speed;
-        currentDirection = 2
-    }
-    requestAnimationFrame(game);
-    drawFrames();
-}
-requestAnimationFrame(game)
+})
